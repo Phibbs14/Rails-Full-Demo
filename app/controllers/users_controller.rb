a86772de4bbca6ac2ff6486579c1d277b6ee74a3
Shane Phibbs <phibbss14@gmail.com>
2014-04-17 16:20:04 -0400

    Completed extra examples for chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-    before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
-    before_action :correct_user,    only: [:edit, :update]
-    before_action :admin_user,      only: :destroy
+    before_action :signed_in_user,    only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,      only: [:edit, :update]
+    before_action :admin_user,        only: :destroy
+    before_action :already_signed_in, only: [:new, :create]
 
     def index
       @users = User.paginate(page: params[:page])
@@ -41,9 +42,14 @@ class UsersController < ApplicationController
     end
 
     def destroy
-      User.find(params[:id]).destroy
-      flash[:success] = "User deleted."
-      redirect_to users_url
+      victim = User.find(params[:id])
+      if current_user?(victim)
+        redirect_to root_url
+      else
+        victim.destroy
+        flash[:success] = "User deleted."
+        redirect_to users_url
+      end
     end
 
   	private
@@ -66,6 +72,10 @@ class UsersController < ApplicationController
       end
 
       def admin_user
-        redirect_to(root_url) unless current_user.admin?        
+        redirect_to(root_url) unless current_user.admin?     
+      end
+
+      def already_signed_in
+        redirect_to(root_url) if signed_in?
       end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,18 +43,17 @@ describe "Authentication" do
             it "should render the desired protected page" do
               expect(page).to have_title('Edit user')
             end
-          end
-        end
 
-        describe "as non-admin user" do
-          let(:user) { FactoryGirl.create(:user) }          
-          let(:non_admin){ FactoryGirl.create(:user) }
+            describe "when signing in again" do
+              before do
+                click_link "Sign out"
+                sign_in user
+              end
 
-          before { sign_in non_admin, no_capybara: true }
-
-          describe "submitting a DELETE request to the Users#destroy action" do
-            before { delete user_path(user) }
-            specify { expect(response).to redirect_to(root_url) }
+              it "should render the default (profile) page" do
+                expect(page).to have_title(user.name)
+              end
+            end
           end
         end
       end
@@ -84,6 +83,8 @@ describe "Authentication" do
   			before { click_button "Sign in" }
 
   			it { should have_title('Sign in') }
+        it { should_not have_link('Profile') }
+        it { should_not have_link('Settings') }
   			it { should have_error_message('Invalid') }
 
   			describe "after visiting another page" do
@@ -110,4 +111,25 @@ describe "Authentication" do
         end
   		end
   	end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }          
+      let(:non_admin){ FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "as admin user" do
+      let(:admin){ FactoryGirl.create(:admin) }
+      before { sign_in admin }
+      describe "attempting to delete oneself" do
+        before { delete user_path(admin) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
 end
